4d656f7db10861fbf91d1ec6c7dd9c0e64d10c74
iliaalii <86724160+iliaalii@users.noreply.github.com>
2025-06-07 18:06:35 +0300

    Update QuickDBD-12_sprint.sql

--- src/ER-diagram/QuickDBD-12_sprint.sql
+++ src/ER-diagram/QuickDBD-12_sprint.sql
@@ -1,90 +1,53 @@
--- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- Link to schema: https://app.quickdatabasediagrams.com/#/d/i89NpA
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+-- Упрощенный вариант. создан исключительно для удобного комментирования ревью :)
 
-
-CREATE TABLE `Films` (
-    `film_id` int  NOT NULL ,
-    `name` text  NOT NULL ,
-    `description` varchar(200)  NULL ,
-    `releaseDate` date  NOT NULL ,
-    `duration` int  NOT NULL ,
-    `rating_id` int  NULL ,
-    PRIMARY KEY (
-        `film_id`
-    )
+CREATE TABLE Films (
+  film_id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  name TEXT NOT NULL,
+  description VARCHAR(200) NOT NULL,
+  release_date DATE NOT NULL,
+  duration INTEGER NOT NULL,
+  rating_id INTEGER NOT NULL
 );
 
-CREATE TABLE `Users` (
-    `user_id` int  NOT NULL ,
-    `login` text  NOT NULL ,
-    `name` text  NULL ,
-    `email` varchar(256)  NOT NULL ,
-    `birthday` date  NOT NULL ,
-    PRIMARY KEY (
-        `user_id`
-    )
+CREATE TABLE Users (
+  user_id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  login TEXT NOT NULL,
+  name TEXT NULL,
+  email VARCHAR(256) NOT NULL,
+  birthday DATE NOT NULL
 );
 
-CREATE TABLE `Friends` (
-    `user_id` int  NOT NULL ,
-    `friend_id` int  NOT NULL ,
-    `confirmed` boolean  NOT NULL ,
-    PRIMARY KEY (
-        `user_id`,`friend_id`
-    )
+CREATE TABLE Friends (
+  id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  friend_id INTEGER NOT NULL,
+  confirmed boolean NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES Users(user_id),
+  FOREIGN KEY (friend_id) REFERENCES Users(user_id)
 );
 
-CREATE TABLE `Likes` (
-    `film_id` int  NOT NULL ,
-    `user_id` int  NOT NULL ,
-    PRIMARY KEY (
-        `film_id`,`user_id`
-    )
+CREATE TABLE Likes (
+  id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  film_id INTEGER NOT NULL,
+  user_id INTEGER NOT NULL,
+  FOREIGN KEY (film_id) REFERENCES Films(film_id),
+  FOREIGN KEY (user_id) REFERENCES Users(user_id)
 );
 
-CREATE TABLE `Rating` (
-    `rating_id` int  NOT NULL ,
-    `name` text  NOT NULL ,
-    PRIMARY KEY (
-        `rating_id`
-    )
+CREATE TABLE Rating (
+  rating_id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  name TEXT NOT NULL
 );
 
-CREATE TABLE `Film_Genre` (
-    `film_id` int  NOT NULL ,
-    `genre_id` int  NOT NULL ,
-    PRIMARY KEY (
-        `film_id`
-    )
+CREATE TABLE Genres (
+  genre_id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  name TEXT NOT NULL
 );
 
-CREATE TABLE `Genre` (
-    `genre_id` int  NOT NULL ,
-    `name` text  NOT NULL ,
-    PRIMARY KEY (
-        `genre_id`
-    )
+CREATE TABLE Films_Genres (
+  id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  film_id INTEGER NOT NULL,
+  genre_id INTEGER NOT NULL,
+  FOREIGN KEY (film_id) REFERENCES Films(film_id),
+  FOREIGN KEY (genre_id) REFERENCES Genres(genre_id)
 );
-
-ALTER TABLE `Films` ADD CONSTRAINT `fk_Films_rating_id` FOREIGN KEY(`rating_id`)
-REFERENCES `Rating` (`rating_id`);
-
-ALTER TABLE `Friends` ADD CONSTRAINT `fk_Friends_user_id` FOREIGN KEY(`user_id`)
-REFERENCES `Users` (`user_id`);
-
-ALTER TABLE `Friends` ADD CONSTRAINT `fk_Friends_friend_id` FOREIGN KEY(`friend_id`)
-REFERENCES `Users` (`user_id`);
-
-ALTER TABLE `Likes` ADD CONSTRAINT `fk_Likes_film_id` FOREIGN KEY(`film_id`)
-REFERENCES `Films` (`film_id`);
-
-ALTER TABLE `Likes` ADD CONSTRAINT `fk_Likes_user_id` FOREIGN KEY(`user_id`)
-REFERENCES `Users` (`user_id`);
-
-ALTER TABLE `Film_Genre` ADD CONSTRAINT `fk_Film_Genre_film_id` FOREIGN KEY(`film_id`)
-REFERENCES `Films` (`film_id`);
-
-ALTER TABLE `Film_Genre` ADD CONSTRAINT `fk_Film_Genre_genre_id` FOREIGN KEY(`genre_id`)
-REFERENCES `Genre` (`genre_id`);
-
